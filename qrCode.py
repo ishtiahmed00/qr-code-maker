44e574c4fdc46306e3aadd5074fadb1eeb75f5b5
ishti-ahmed <ishtiahmed191@gmail.com>
2025-11-29 21:46:53 +0600

    qrcode sucessed

---
+++ qrCode.py
@@ -0,0 +1,19 @@
+# import qrcode as qr
+# img = qr.make("https://youtu.be/Fj6cr3FO2JI?si=kkNHIZfoxDyN404J")
+# img.save("Ishti_youtube.png")
+
+import qrcode
+
+img = qrcode.make("https://youtu.be/Fj6cr3FO2JI?si=kkNHIZfoxDyN404J")
+img.save("hello_ji.png")
+
+
+print("My Name is Khan")
+print("My Name is kana")
+
+
+print("faijlami")
+
+print("suuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu")
+print("ok")
+    
